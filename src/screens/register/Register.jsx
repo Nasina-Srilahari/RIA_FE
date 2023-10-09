44e379753fb8160d7c039382e0c686126d4c8de8
teju-loki2 <tejaswini.yallamraju2@gmail.com>
2023-10-09 13:52:43 +0530

    register

--- src/screens/register/Register.jsx
+++ src/screens/register/Register.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React from 'react';
+import "./register.css";
+import {BiLogoGmail} from "react-icons/bi"
+import {AiFillLock} from "react-icons/ai"
+import {BiSolidUser} from "react-icons/bi"
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div class="wrapper">
+
+     <form action="">
+        <h1>REGISTER</h1>
+        <div class="input-box">
+          <input type="text" placeholder="Full Name" required/>
+          <i><BiSolidUser/></i>
+          </div>
+        <div class="input-box">
+          <input type="text" placeholder="Email" required/>
+          <i><BiLogoGmail/></i>
+        </div>
+        <div class="input-box">
+          <input type="password" placeholder="password" required/>
+          <i><AiFillLock/></i>
+        </div>
+        <div class="input-box">
+          <input type="Confirm password" placeholder="Confirm password" required/>
+          <i><AiFillLock/></i>
+        </div>
+        
+        <button type="submit" class="btn">Register</button>
+        <div class="login-link">
+          <p>Have an account <a href="#">Login</a> </p>
+        </div>
+      </form>
+    </div>
   )
 }
 
