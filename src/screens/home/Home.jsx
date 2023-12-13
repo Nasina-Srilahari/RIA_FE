cb8d9c3490d2c5b27144fe81f786db04df5afa07
Nasina-Srilahari <nasinasrilahariias@gmail.com>
2023-12-13 10:09:51 +0530

    fav and search fun

--- src/screens/home/Home.jsx
+++ src/screens/home/Home.jsx
@@ -11,56 +11,74 @@ import { useEffect } from 'react';
 import api from '../../api/api';
 
 const Home = () => {
-  const [sidebar, setSidebar] = useState(false)
-  library.add(faSearch)
+  const [sidebar, setSidebar] = useState(false);
+  const [user, setUser] = useState(localStorage.getItem("user"));
+  const [img, setImage] = useState("");
+  const [books, setBooks] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
-  const [user, setUser] = useState(localStorage.getItem("user"))
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const [img, setImage] = useState("")
-  const [books, setBooks] = useState([])
+  library.add(faSearch);
 
-  console.log(user)
-
-  const fetchBooks= () => {
-    api.post('book/ret-book',
-      ).then(response => {
-      console.log(response.data)
-      setBooks(response.data.books)
-      setImage(response.data.img)
-    })
-  }
+  const fetchBooks = () => {
+    api.post('book/ret-book').then(response => {
+      console.log(response.data);
+      setBooks(response.data.books);
+      setImage(response.data.img);
+    });
+  };
 
   useEffect(() => {
-    if(user === null || undefined){
-      navigate("/")
+    if (user === null || undefined) {
+      navigate("/");
     }
-    fetchBooks()
-  },[])
+    fetchBooks();
+  }, []);
 
-  
+  const handleSearch = () => {
+    // Perform the search based on name and price
+    // You can modify the API request according to your backend API
+    api.post('book/search', { searchTerm }).then(response => {
+      console.log(response.data);
+      setBooks(response.data.books);
+      setImage(response.data.img);
+      console.log(response.data.img)
+    });
+  };
+
   return (
     <div>
       <div>
-        <Navbar visibility={{sidebar, setSidebar}}/>
+        <Navbar visibility={{ sidebar, setSidebar }} />
       </div>
-      <div style={{marginLeft: sidebar == true ? "230px" : "10px", paddingTop: "80px"}}>
+      <div style={{ marginLeft: sidebar ? "230px" : "10px", paddingTop: "80px" }}>
         <div className='search-div'>
-          <FontAwesomeIcon icon="fa-solid fa-search"/>
-          <input className='search-bar' id='search-bar' name='search-bar' placeholder='Search....'/>
+          <FontAwesomeIcon icon={faSearch} />
+          <input
+            className='search-bar'
+            id='search-bar'
+            name='search-bar'
+            placeholder='Search....'
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button className="search-btn" onClick={handleSearch}>Search</button>
           <div className='books-cards-buy-wrapper'>
-            {
-              books.map(function(book,i){
-                return(
-                  <BookCards book={book} img={`data:image/png;base64,${img[i]}`} />
-                )
-              })
-            }
+              {books && books.length > 0 ? (
+                books.map((book, i) => (
+                  <BookCards key={i} book={book} img={img[i] ? `data:image/png;base64,${img[i]}` : ''} />
+                ))
+              ) : (
+                <p>No books found.</p>
+              )}
           </div>
+
+
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
